a66cc83b99b0debe8aa1db41b43cd46547cccc51
mbaidavis <90491095+Mbai2000@users.noreply.github.com>
2022-05-24 22:29:56 -0700

    Minutes and Seconds Timer
    Minutes and Seconds Timer

--- CrunchTime/Assets/Scripts/Timer.cs
+++ CrunchTime/Assets/Scripts/Timer.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -7,7 +9,7 @@ using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
     private float currentTime = 0.0f;
-    [SerializeField] private float totalTime = 10.0f;
+    [SerializeField] private float totalTime = 600.0f;
 
     // Referenced https://www.youtube.com/watch?v=o0j7PdU88a4&t=215s in order to make UI element update in canvas.
     [SerializeField] Text timerText;
@@ -29,9 +31,13 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        currentTime -= 1 * Time.deltaTime;
-        timerText.text = currentTime.ToString("0");
-        if (currentTime <= 0)
+        currentTime -= Time.deltaTime;
+        var minuteTime = Mathf.FloorToInt(currentTime/60);
+        var secondTime = Mathf.FloorToInt(currentTime % 60);
+        // Referenced https://answers.unity.com/questions/45676/making-a-timer-0000-minutes-and-seconds.html in order to make the text appear properly
+        var timeString = minuteTime.ToString("00") + ":" + secondTime.ToString("00");
+        timerText.text = timeString;
+        if (currentTime <=-180)
         {
             SceneManager.LoadScene("GameOver");
         }
