a2ad4b54551f479e194faff3ff4d43193362dfe3
khansahab117 <aaakhan@ucdavis.edu>
2022-05-28 17:13:44 -0700

    implemented a door system

---
+++ CrunchTime/Assets/Scripts/DoorController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField]
+    public Tilemap doorBlock;
+
+    //create a list of boolean for each level
+    public Bool NewLevelUnlocked;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorBlock = GetComponent<Tilemap>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Projectile"){
+             Debug.Log("INsideeee");
+            Vector3 positionCollided = Vector3.zero;
+            foreach(ContactPoint2D hit in collision.contacts)
+            {
+                positionCollided.x = hit.point.x - 0.01f *hit.normal.x;
+                positionCollided.y = hit.point.y - 0.01f *hit.normal.y;
+                doorBlock.SetTile(doorBlock.WorldToCell(positionCollided),null);
+            }
+        }
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
